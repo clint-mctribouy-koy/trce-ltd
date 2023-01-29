6a549f1f163c35727de06ddbc2a8ae4275e5c95a
clint-mctribouy-koy <clintmctribouy@outlook.com>
2023-01-29 19:39:18 +0000

    cretaed custom user model

--- app/app/settings.py
+++ app/app/settings.py
@@ -141,7 +141,7 @@ MEDIA_URL = '/media/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# AUTH_USER_MODEL = 'core.User'
+AUTH_USER_MODEL = 'core.UserAccount'
 
 
 
--- app/core/models.py
+++ app/core/models.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 import uuid
 import os
 from django.utils import timezone
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
 
 
 def product_image_file_path(instance, filename):
@@ -26,25 +27,67 @@ LABEL = (
     ('TR', 'Top Rated')
 )
 
+
+class UserAccountManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('Users must have an email address')
+
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+
+        user.set_password(password)
+        user.save()
+
+        return user
+    def create_superuser(self, email, password):
+        """Create and return a new superuser."""
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save()
+
+        return user
+
+class UserAccount(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(max_length=255, unique=True)
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserAccountManager()
+
+    USERNAME_FIELD = 'email'
+
+    def get_full_name(self):
+        return self.first_name
+
+    def get_short_name(self):
+        return self.first_name
+    
+    def __str__(self):
+        return self.email
+
 class Product(models.Model):
-    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    user = models.ForeignKey(UserAccount, on_delete=models.SET_NULL, null=True)
     name = models.CharField(max_length=200, null=True, blank=True)
     image = models.ImageField(null=True, blank=True,  default='/placeholder.png')
     brand = models.CharField(max_length=200, null=True, blank=True)
     category = models.CharField(max_length=200, null=True, blank=True)
     description = models.TextField(null=True, blank=True)
-    # numReviews = models.IntegerField(null=True, blank=True, default=0)
+    numReviews = models.IntegerField(null=True, blank=True, default=0)
     price = models.DecimalField(
         max_digits=7, decimal_places=2, null=True, blank=True)
-    # countInStock = models.IntegerField(null=True, blank=True, default=0)
+    countInStock = models.IntegerField(null=True, blank=True, default=0)
     createdAt = models.DateTimeField(default=timezone.now)
-    uuid = models.UUIDField(primary_key=True, editable=False,  db_index=True, default=uuid.uuid4())
+    _id = models.BigAutoField(primary_key=True, editable=False)
 
     def __str__(self):
         return self.name
 
 class Order(models.Model):
-    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    user = models.ForeignKey(UserAccount, on_delete=models.SET_NULL, null=True)
     paymentMethod = models.CharField(max_length=200, null=True, blank=True)
     taxPrice = models.DecimalField(
         max_digits=7, decimal_places=2, null=True, blank=True)
@@ -58,58 +101,9 @@ class Order(models.Model):
     deliveredAt = models.DateTimeField(
         auto_now_add=False, null=True, blank=True)
     createdAt = models.DateTimeField(default=timezone.now)
-    uuid = models.UUIDField(primary_key=True, editable=False,  db_index=True, default=uuid.uuid4())
+    _id = models.BigAutoField(primary_key=True, editable=False)
 
     def __str__(self):
         return str(self.createdAt)
-
-# class OrderItem(models.Model):
-#     product = models.ForeignKey(Item, on_delete=models.SET_NULL, null=True)
-#     order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True)
-#     name = models.CharField(max_length=200, null=True, blank=True)
-#     qty = models.IntegerField(null=True, blank=True, default=0)
-#     price = models.DecimalField(max_digits=7, decimal_places=2, null=True, blank=True)
-#     image = models.CharField(max_length=200, null=True, blank=True)
-#     uuid = models.UUIDField(default=uuid.uuid4(), primary_key=True, db_index=True, editable=False)
-
-#     def __str__(self):
-#         return str(self.name)
-
-# class Review(models.Model):
-#     product = models.ForeignKey(Item, on_delete=models.SET_NULL, null=True)
-#     user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
-#     name = models.CharField(max_length=200, null=True, blank=True)
-#     rating = models.IntegerField(null=True, blank=True, default=0)
-#     comment = models.TextField(null=True, blank=True)
-#     createdAt = models.DateTimeField(default=timezone.now)
-#     uuid = models.UUIDField(default=uuid.uuid4(), primary_key=True, db_index=True, editable=False)
-
-#     def __str__(self):
-#         return str(self.rating)
-
-# class ShippingAddress(models.Model):
-#     order = models.OneToOneField(Order, on_delete=models.CASCADE, null=True, blank=True)
-#     address = models.CharField(max_length=200, null=True, blank=True)
-#     city = models.CharField(max_length=200, null=True, blank=True)
-#     postalCode = models.CharField(max_length=200, null=True, blank=True)
-#     country = models.CharField(max_length=200, null=True, blank=True)
-#     shippingPrice = models.DecimalField(
-#         max_digits=7, decimal_places=2, null=True, blank=True)
-#     uuid = models.UUIDField(default=uuid.uuid4(), primary_key=True, db_index=True, editable=False)
-
-#     def __str__(self):
-#         return str(self.address)
-
-
-
-
-# class BrandStore(models.Model):
-#     name = models.CharField(max_length=255)
-#     description = models.CharField(max_length=255)
-#     products = models.ForeignKey('Product', on_delete=models.CASCADE)
-
-#     def __str__(self):
-#         return self.name
-
 
 
